aa90809d0c95d350b62b575f8279d2de0492bc73
kawakami <daidai1204yade@gmail.com>
2024-07-17 15:04:31 +0900

    renderメソッドを使ってserverとhtmlファイルの連携

--- server.py
+++ server.py
@@ -2,27 +2,27 @@ import tornado.ioloop#tornadoイベントグループを管理するモジュー
 #サーバの起動と非同期処理の核
 import tornado.web#tornadoのWebフレームワーク機能を提供モジュール
 #リクエストハンドラ、ルーティングなどを含む
+import os
 
 #tornado.webの.RequestHandlerを継承　実際アクセスしたら表示される部分
 class MainHandler(tornado.web.RequestHandler):
     def get(self):#getリクエストを処理
-        self.write("""
-        <html><body><form action="/" method="post">
-        <input type="text" name="input">
-        <input type="submit" value="Submit">
-        </form></body></html>
-        """)
+        self.render("index.html")#render関数はtornadoのrequestHandlerクラスのメソッド
+        #テンプレートファイルを読み込んで、HTMLとしてクライアントに送信できる。（必要なデータだけ選ぶことも可能）
 
     def post(self):
         user_input = self.get_body_argument("input", default="No input received")
-        self.write(f"Received input: {user_input}")
+        self.write(f"Received input: {user_input}")#入力されたinputの中身を表示、なければno input received
 
 
 #tornado.web.のApplicationクラスのインスタンスを作成
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),#ルートパスと対応するハンドラをマッピング
-    ])
+        #メインハンドラがそのままなら、/、違うならまた別で違う対応関数を書いてあげる
+    ],
+    template_path=os.path.join(os.path.dirname(__file__),"Web_page")#renderで指定するテンプレートフォルダの場所の初期設定
+    )
 
 if __name__ == "__main__":
     app = make_app()
